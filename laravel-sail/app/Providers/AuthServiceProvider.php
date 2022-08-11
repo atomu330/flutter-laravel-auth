16e846a95b6c65196784ebea939cad6582fefc5a
atomu330 <atomu.work@gmail.com>
2022-08-11 13:21:08 +0900

    use文の追加

--- laravel-sail/app/Providers/AuthServiceProvider.php
+++ laravel-sail/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport; // 追加
 
 class AuthServiceProvider extends ServiceProvider
 {
